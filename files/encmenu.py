bb1b8c5244cd197f29305707fcf1c2cdec048845
Aniket-PC <prosniperaniket@gmail.com>
2019-03-26 03:09:30 +0530

    added features

--- files/encmenu.py
+++ files/encmenu.py
@@ -1,22 +1,34 @@
 from tkinter import *
+from files.msgencryption.files2.reversecipher import *
+from functools import partial
+
 
 def create_enc():
     window=Toplevel()
     window.title("Encryption")
     window.geometry("200x300+270+50")
-    l1 = Label(window, text="Text Encryption Algorithm's")
 
-    b1 = Button(window, text="Reverse Cipher")
+    e1 = Entry(window, bd=5)
+    e1.pack()
+
+    b1f=partial(revc,window,e1)
+    l1 = Label(window, text="Text Encryption Algorithm's")
+    b1 = Button(window, text="Reverse Cipher",command=b1f)
     b2 = Button(window,text="Transposition Cipher")
     b3= Button(window, text ="base64 cipher")
     b4 = Button(window, text="File Encryption")
     b5 = Button(window, text="LSB Encryption")
 
+
     l1.pack()
+
+    #e1.pack()
 
     b2.pack(fill=X,padx=20)
     b3.pack(fill=X,padx=20)
     b1.pack(fill=X,padx=20)
     b4.pack(fill=X,padx=20)
     b5.pack(fill=X,padx=20)
+
+
 
---
+++ files/msgencryption/files2/reversecipher.py
@@ -0,0 +1,18 @@
+from tkinter import *
+import tkinter.messagebox
+from pyperclip import copy
+
+def revc(master,e1):
+    message=e1.get()
+    crev=''
+    i=len(message)-1
+    while i>=0:
+        crev=crev+message[i]
+        i=i-1
+    copy(crev)
+    l2=Label(master,text=crev)
+    l2.pack()
+    tkinter.messagebox.showinfo("NOTICE","The result is copied in Clipboard")
+
+
+
