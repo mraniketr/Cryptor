2c540c6e9a7101a234805a216ffd97da1766ee78
Aniket-PC <prosniperaniket@gmail.com>
2019-03-26 20:34:23 +0530

    DATA HIDE implemented

---
+++ files/dataencryption/dataHIDE.py
@@ -0,0 +1,44 @@
+#to run system commands
+import os
+#to make archive
+import shutil
+#Filedialog for selection
+from tkinter import *
+from tkinter import filedialog
+
+
+#to distinguish between OS
+import sys
+
+
+#function to unzip the encrypted image with classified information
+def fuzip():
+    if(sys.platform=='win32'):
+        os.system('rename classified.jpg classified.rar')
+        os.system('7z.exe x classified.rar')
+    else:
+        os.system("unzip classified.jpg")
+    print("Extraction successful")
+
+#function to create zip and the encrypted image with classified information
+def fzip():
+    root=Tk()
+    root.update()
+    shutil.make_archive('final','zip',root_dir=filedialog.askdirectory(title = "Open directory containg your files"),base_dir=None)
+    root.destroy()
+
+    if(sys.platform=='win32'):
+        isrc=filedialog.askopenfilename()
+        os.rename(isrc,'final.jpg')
+        os.system("copy /b final.jpg+final.zip classified.jpg")
+        os.system("del final.zip")
+        os.system("del final.jpg")
+
+    else:
+        isrc=filedialog.askopenfilename()
+        os.rename(isrc,'final.jpg')
+        os.system("cat final.jpg final.zip > classified.jpg")
+        os.system("rm final.zip")
+
+
+
--- files/encmenu.py
+++ files/encmenu.py
@@ -4,6 +4,8 @@ from functools import partial
 from files.msgencryption.basesix4 import *
 from files.msgencryption.transpostion import *
 from files.dataencryption.fileselector import *
+from files.dataencryption.dataHIDE import *
+
 
 def create_enc():
     window=Toplevel()
@@ -19,6 +21,7 @@ def create_enc():
     b1f=partial(revc,window,e1)
     b2f=partial(b64enc, window, e1)
     b3f=partial(tcenc,window,e1,e2)
+    b4f=partial(fzip)
     b5f=partial(selection)
 
 
@@ -26,7 +29,7 @@ def create_enc():
     b1 = Button(window, text="Reverse Cipher",command=b1f)
     b2 = Button(window,text="Transposition Cipher",command=b3f)
     b3= Button(window, text ="base64 cipher", command=b2f)
-    b4 = Button(window, text="File Encryption")
+    b4 = Button(window, text="File Encryption",command=b4f)
     b5 = Button(window, text="LSB Encryption",command=b5f)
 
 
